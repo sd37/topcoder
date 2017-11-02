598c44a88d2b12469250bf710327913104c8a9ea
spandan <spandan@prosperworks.com>
2017-11-01 22:47:02 -0700

    more stuff added

--- SRM 701 DIV 2/src/SortingSubsets.java
+++ SRM 701 DIV 2/src/SortingSubsets.java
@@ -7,13 +7,13 @@ import static java.lang.Math.*;
 public class SortingSubsets {
 
   public int getMinimalSize(int[] a) {
-		int[] copyA = Arrays.copyOf(a, a.length);
+    int[] copyA = Arrays.copyOf(a, a.length);
     Arrays.sort(a);
 
     int diff = 0;
 
-    for(int i = 0; i < a.length; i++) {
-      if(a[i] != copyA[i]) {
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] != copyA[i]) {
         diff++;
       }
     }
